3aec1f165eb5f75ffb2c887062039e867a776ced
Benjamn <bfreeman@lcs.on.ca>
2021-02-18 20:40:44 -0500

    Fatal error, with the array

--- RSANumbers/main.swift
+++ RSANumbers/main.swift
@@ -21,41 +21,41 @@ print("===========")
 // INPUT
 
 // Get lower limit of range
-print("Enter lower limit of range")
-let lower = Int(readLine()!)!
+let lower = Int.collectInput(withPrompt: "Enter lower limit of range", minimum: nil, maximum: nil)
 
 // Get upper limit of range
-print("Enter upper limit of range")
-let upper = Int(readLine()!)!
+let upper = Int.collectInput(withPrompt: "Enter upper limit of range", minimum: nil, maximum: nil)
 
 // PROCESS
+// do the math for the upper and lower
 
 // Count the number of divisors in the given Number
-func divisorCount(of givenNumber: Int) -> Int {
+func countOfRSANumbersFrom(lower: Int, upper: Int) -> String {
+
     
     // 1 is always a divisor
     var divisorCount = 1
     
     // Start looping from 2
-    for i in 2...givenNumber {
+    for i in 1...100 {
         
         // What is the remainder?
-        let remainder = givenNumber % i
+        let remainder: [Int] = Array(lower...upper % i)
         
         // DEBUG output...
-        print("\(givenNumber) \\ \(i) has a remainder of \(remainder)")
+        print("\(lower...upper) \\ \(i) has a remainder of \(remainder)")
         
         // TODO: Add some code here... we need to keep track of how many divisors there are
-        if remainder == 0 {
+        if remainder == [0] {
+             divisorCount += 1
             
         }
         
     }
-    
     // Return the count of divisors
-    return divisorCount
-    
+    return     "The number of RSA numbers between \(lower) and \(upper) is \(divisorCount)"
 }
 
 // How many divisors does a number have?
-print("5 has this many divisors... \(divisorCount(of: 5))")
+let drive = countOfRSANumbersFrom(lower: lower, upper: upper)
+print(drive)
